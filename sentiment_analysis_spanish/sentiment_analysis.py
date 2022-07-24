0d10926bdb580965102c3650935b3aae63b2b10c
Luis Jiménez <luisjimenez6245@hotmail.com>
2022-07-24 14:08:33 -0500

    update files

--- sentiment_analysis_spanish/sentiment_analysis.py
+++ sentiment_analysis_spanish/sentiment_analysis.py
@@ -1,39 +1,40 @@
-
-
-
-from sklearn.metrics import confusion_matrix, f1_score
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-
-import re
+import _pickle as pickle
 import bz2
-import _pickle as cPickle
+import re
 from pathlib import Path
+
 REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
 BAD_SYMBOLS_RE = re.compile('[^\w\s]')
 
+
 def compressed_pickle(title, data):
     with bz2.BZ2File(title + '.pbz2', 'w') as f:
-        cPickle.dump(data, f)
+        pickle.dump(data, f)
+
 
 def decompress_pickle(file):
     data = bz2.BZ2File(file, 'rb')
-    data = cPickle.load(data)
+    data = pickle.load(data)
     return data
+
+
+def clean_text(text):
+    text = text.lower()
+    text = REPLACE_BY_SPACE_RE.sub(' ', text)
+    text = BAD_SYMBOLS_RE.sub('', text)
+    return text
+
 
 class SentimentAnalysisSpanish:
     def __init__(self):
         self.parent_path = Path(__file__).parent
-        self.vectorizer = decompress_pickle(str(self.parent_path / 'saved_model/ngram_vectorized_compressed.pbz2'))
-        self.classifier = decompress_pickle(str(self.parent_path / 'saved_model/classifier_naive_bayes_compressed.pbz2'))
+        path_vectorizer = str(self.parent_path / 'saved_model/ngram_vectorized_compressed.pbz2')
+        self.vectorizer = decompress_pickle(path_vectorizer)
+        path_classifier = str(self.parent_path / 'saved_model/classifier_naive_bayes_compressed.pbz2')
+        self.classifier = decompress_pickle(path_classifier)
 
-    def clean_text(self, text):
-        text = text.lower()
-        text = self.REPLACE_BY_SPACE_RE.sub(' ',text)
-        text = self.BAD_SYMBOLS_RE.sub('', text)
-        return text
 
-    #returns the sentiment of a text string
-    def sentiment(self, text:str):
+    # returns the sentiment of a text string
+    def sentiment(self, text: str):
         vals = self.vectorizer.transform([text])
         return self.classifier.predict_proba(vals)[0][1]
